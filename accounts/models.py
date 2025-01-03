6d69b8687472ef117cabc7f226c6d1701e3d67b9
Keito Ogawa~ <qingshixiaochuan24@gmail.com>
2025-01-03 22:03:32 +0900

    カスタムユーザーモデルの実装

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,16 @@
 
 
 # class FriendShip(models.Model):
+
+
+from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .models import User
+
+admin.site.register(User)
+
+
+class User(AbstractUser):
+    email = models.EmailField()
--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "welcome.apps.WelcomeConfig",
 ]
 
+# 追加認証システムの設定
+
+AUTH_USER_MODEL = "accounts.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -124,5 +128,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "accounts.User"
