cfab0236af90c73860523fbca08bcbe6ff8349dd
Keito Ogawa~ <qingshixiaochuan24@gmail.com>
2025-01-04 00:02:22 +0900

    修正

--- accounts/models.py
+++ accounts/models.py
@@ -8,13 +8,8 @@
 # class FriendShip(models.Model):
 
 
-from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-from .models import User
-
-admin.site.register(User)
 
 
 class User(AbstractUser):
